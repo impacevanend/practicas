8b1cfaafae951ca9b40d93b254f1f94b677e69fe
impacevanend <54188811+impacevanend@users.noreply.github.com>
2020-01-07 00:59:45 -0500

    version final

---
+++ introducciòn.js
@@ -0,0 +1,29 @@
+/*
+
+TODO ¿Porque es importante Javascript?
+Lenguaje màs utilizado para la programaciòn web
+*Características:
+
+-El lenguaje se ejecuta en el navegador
+-Es un lenguaje interpretado, el navegador interpreta
+ sentencia a sentencia del código.
+-Es un lenguaje orientado a objetos y badado
+ en prototipos.
+-Es un lenguaje imperativo y estructurado.
+imperativo recibe las ordenes del desarrollador por
+medio de las instrucciones,
+*Estructuras basicas
+Secuencia
+Selección
+Iteración
+
+-Tiene sus propios objetocos, con atributos y metodos
+tipificación debil(no es necesario indicar de que 
+    tipo van a ser las variables) y dinamica,(la variable
+    puede ser un entero, string etc.)
+
+
+
+
+
+*/
